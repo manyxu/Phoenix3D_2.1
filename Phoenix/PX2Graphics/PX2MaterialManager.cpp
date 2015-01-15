0eae2d48009092c114c8bc5093cdd96bb4fcb234
linkjoy <realmany@163.com>
2015-01-15 21:29:47 +0800

    change mtl system

---
+++ Phoenix/PX2Graphics/PX2MaterialManager.cpp
@@ -0,0 +1,77 @@
+// PX2MaterialManager.cpp
+
+#include "PX2MaterialManager.hpp"
+#include "PX2GraphicsRoot.hpp"
+#include "PX2PVMatrixConstant.hpp"
+#include "PX2PVWMatrixConstant.hpp"
+using namespace PX2;
+
+//----------------------------------------------------------------------------
+MaterialManager::MaterialManager() :
+mNextShaderKey(1000)
+{
+}
+//----------------------------------------------------------------------------
+MaterialManager::~MaterialManager()
+{
+}
+//----------------------------------------------------------------------------
+bool MaterialManager::Initlize()
+{
+	mNextShaderKey = 1000;
+
+	return true;
+}
+//----------------------------------------------------------------------------
+bool MaterialManager::Terminate()
+{
+	mMaterialMap.clear();
+
+	return true;
+}
+//----------------------------------------------------------------------------
+Material *MaterialManager::GetMaterial(const FString &filename)
+{
+	Material *mtl = 0;
+	std::map<FString, MaterialPtr>::iterator it = mMaterialMap.find(filename);
+	if (it != mMaterialMap.end())
+	{
+		mtl = it->second;
+	}
+	else
+	{
+		mtl = DynamicCast<Material>(GraphicsRoot::msUserLoadFun(filename));
+		mMaterialMap[filename] = mtl;
+
+		int numTechniques = mtl->GetNumTechniques();
+		for (int i = 0; i < numTechniques; i++)
+		{
+			MaterialTechnique *tech = mtl->GetTechnique(i);
+			int numPass = tech->GetNumPasses();
+			for (int i = 0; i < numPass; i++)
+			{
+				MaterialPass *mtlPass = tech->GetPass(i);
+				VertexShader *vs = mtlPass->GetVertexShader();
+				PixelShader *ps = mtlPass->GetPixelShader();
+
+				vs->SetShaderKey(mNextShaderKey);
+				ps->SetShaderKey(mNextShaderKey);
+
+				mNextShaderKey++;
+			}
+		}
+	}
+
+	return mtl;
+}
+//----------------------------------------------------------------------------
+ShaderFloat *MaterialManager::CreateShaderFloat(const FString &name)
+{
+	if (FString("PVWMatrix") == name)
+	{
+		return new0 PVWMatrixConstant();
+	}
+
+	return 0;
+}
+//----------------------------------------------------------------------------
---
+++ Phoenix/PX2Graphics/PX2MaterialManager.hpp
@@ -0,0 +1,34 @@
+//  PX2MaterialManager.hpp
+
+#ifndef PX2MATERIALMANAGER_HPP
+#define PX2MATERIALMANAGER_HPP
+
+#include "PX2Singleton_NeedNew.hpp"
+#include "PX2Material.hpp"
+#include "PX2ShaderFloat.hpp"
+#include "PX2FString.hpp"
+
+namespace PX2
+{
+
+	class MaterialManager : public Singleton<MaterialManager>
+	{
+	public:
+		MaterialManager();
+		virtual ~MaterialManager();
+
+		bool Initlize();
+		bool Terminate();
+
+		Material *GetMaterial(const FString &filename);
+		ShaderFloat *CreateShaderFloat(const FString &name);
+
+	protected:
+		std::map<FString, MaterialPtr> mMaterialMap;
+		int mNextShaderKey;
+	};
+#define PX2_MATERIALMAN MaterialManager::GetSingleton()
+
+}
+
+#endif
