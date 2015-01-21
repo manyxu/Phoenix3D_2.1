a448717d53c8ae0e624671b605080fc9acd314f4
linkjoy <realmany@163.com>
2015-01-21 18:23:52 +0800

    lua

--- Phoenix/PX2ToLua/PX2ToLua.hpp
+++ Phoenix/PX2ToLua/PX2ToLua.hpp
@@ -11,18 +11,7 @@
 #include "PX2Mathematics.hpp"
 #include "PX2Graphics.hpp"
 #include "PX2Renderers.hpp"
-#include "PX2EventWorld.hpp"
-#include "PX2Input.hpp"
 #include "PX2Unity.hpp"
-#include "PX2SoundSystem.hpp"
-#include "PX2UserInterface.hpp"
-#include "PX2Terrains.hpp"
-#include "PX2Effect.hpp"
-#include "PX2Game.hpp"
-
-// the game
-#include "X_Heads.hpp"
-
 using namespace PX2;
 
 #define PX2_LUA
@@ -33,14 +22,9 @@ using namespace PX2;
 #pragma warning (disable:4800)
 #endif
 
-extern "C"
-{
-#include "lua.h"
-#include "lauxlib.h"
-#include "lualib.h"
-}
-#include "tolua++.h"
+struct lua_State;
 extern int  tolua_PX2_open (lua_State* tolua_S);
+
 #endif
 
 #endif
