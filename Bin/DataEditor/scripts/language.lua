85f613964912f8b525bcf451f5a43ecd5bf6755b
keguanqiang <keguanqiang@163.com>
2015-01-22 18:47:01 +0800

    编辑器菜单提交

--- Bin/DataEditor/scripts/language.lua
+++ Bin/DataEditor/scripts/language.lua
@@ -1,6 +1,7 @@
 -- language.lua
 
 function AddLanguages()
+	--文件菜单
 	PX2_LM:AddItem1("File", "文件")
 	PX2_LM:AddItem1("NewProject", "新建工程")
 	PX2_LM:AddItem1("NewScene", "新建场景")
@@ -9,14 +10,38 @@ function AddLanguages()
 	PX2_LM:AddItem1("SaveAs", "另存为")
 	PX2_LM:AddItem1("Close", "关闭")
 	PX2_LM:AddItem1("Exit", "退出")
-	
+	--编辑菜单
 	PX2_LM:AddItem1("Edit", "编辑")
-	
+	PX2_LM:AddItem1("Repeal", "撤销")
+	PX2_LM:AddItem1("Repeat", "重复")
+	PX2_LM:AddItem1("Copy", "复制")
+	PX2_LM:AddItem1("Paste", "粘贴")
+	PX2_LM:AddItem1("Delete", "删除")
+	PX2_LM:AddItem1("Select", "选择")
+	PX2_LM:AddItem1("Move", "移动")
+	PX2_LM:AddItem1("Rotate", "旋转")
+	PX2_LM:AddItem1("Scale", "缩放")
+	PX2_LM:AddItem1("PlayAndStop", "播放/停止")
+	PX2_LM:AddItem1("ResetPlay", "重置播放")	
+	--视图菜单
 	PX2_LM:AddItem1("View", "视图")
+	PX2_LM:AddItem1("Window", "窗口")
+	PX2_LM:AddItem1("Stage", "舞台")
 	
+	PX2_LM:AddItem1("StageCenter", "舞台中心停靠")
+	PX2_LM:AddItem1("PerspectiveCamera", "使用透视相机")
+	PX2_LM:AddItem1("TopViewCamera", "使用顶视相机")
+	--调试菜单
 	PX2_LM:AddItem1("Debug", "调试")
-	
+	PX2_LM:AddItem1("EditorSimulate", "在编辑器中模拟")
+	PX2_LM:AddItem1("EditorPlay", "编辑器中播放")
+	PX2_LM:AddItem1("Operation", "运行")
+	PX2_LM:AddItem1("PlayConfig", "播放配置")
+	--工具菜单
 	PX2_LM:AddItem1("Tool", "工具")
+	PX2_LM:AddItem1("VersionWindows", "生成版本信息_Windows")
+	PX2_LM:AddItem1("VersionAndroid", "生成版本信息_Android")
+	PX2_LM:AddItem1("VersionIOS", "生成版本信息_IOS")
 	
 	PX2_LM:AddItem1("Help", "帮助")
 end
--- Bin/DataEditor/scripts/start.lua
+++ Bin/DataEditor/scripts/start.lua
@@ -20,15 +20,41 @@ function CreateMainMenu()
 	E_MainFrame:AddMenuItem(itemFile, PX2_LM:GetValue("Close"), "OnCloseScene")
 	E_MainFrame:AddSeparater(itemFile)
 	E_MainFrame:AddMenuItem(itemFile, PX2_LM:GetValue("Exit"), "OnExit")
-	
+	--edit
 	local itemEdit = E_MainFrame:AddMainMenuItem((PX2_LM:GetValue("Edit")))
-	
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Repeal").."\tCtrl-Z", "OnRepeal")
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Repeat").."\tCtrl-Y", "OnRepeat")
+	E_MainFrame:AddSeparater(itemEdit)
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Copy").."\tCtrl-C", "OnCopy")
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Paste").."\tCtrl-V", "OnPaste")
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Delete").."\tDelete", "OnDelete")
+	E_MainFrame:AddSeparater(itemEdit)
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Select").."\tF1", "OnSelect")
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Move").."\tF2", "OnMove")
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Rotate").."\tF3", "OnRotate")
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("Scale").."\tF4", "OnScale")
+	E_MainFrame:AddSeparater(itemEdit)
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("PlayAndStop").."\tAlt-Space", "OnPlayAndStop")
+	E_MainFrame:AddMenuItem(itemEdit, PX2_LM:GetValue("ResetPlay").."\tShift-Space", "OnResetPlay")
+	--itemview
 	local itemView = E_MainFrame:AddMainMenuItem((PX2_LM:GetValue("View")))
-	
+	E_MainFrame:AddMenuItem(itemView, PX2_LM:GetValue("Window"), "OnWindow")
+	E_MainFrame:AddMenuItem(itemView, PX2_LM:GetValue("StageCenter").."\tCtrl-Enter", "OnStageCenter")
+	E_MainFrame:AddSeparater(itemView)
+	E_MainFrame:AddMenuItem(itemView, PX2_LM:GetValue("PerspectiveCamera").."\tF5", "OnPerspectiveCamera")
+	E_MainFrame:AddMenuItem(itemView, PX2_LM:GetValue("TopViewCamera").."\tF6", "OnTopViewCamera")
+	--debug
 	local itemDebug = E_MainFrame:AddMainMenuItem((PX2_LM:GetValue("Debug")))
-	
+	E_MainFrame:AddMenuItem(itemDebug, PX2_LM:GetValue("EditorSimulate"), "OnEditorSimulate")
+	E_MainFrame:AddMenuItem(itemDebug, PX2_LM:GetValue("EditorPlay"), "OnEditorPlay")
+	E_MainFrame:AddMenuItem(itemDebug, PX2_LM:GetValue("Operation").."\tCtrl-G", "OnOperation")
+	E_MainFrame:AddMenuItem(itemDebug, PX2_LM:GetValue("PlayConfig"), "OnPlayConfig")
+	--tool
 	local itemTool = E_MainFrame:AddMainMenuItem((PX2_LM:GetValue("Tool")))
-	
+	E_MainFrame:AddMenuItem(itemTool, PX2_LM:GetValue("VersionWindows"), "OnVersionWindows")
+	E_MainFrame:AddMenuItem(itemTool, PX2_LM:GetValue("VersionAndroid"), "OnVersionAndroid")
+	E_MainFrame:AddMenuItem(itemTool, PX2_LM:GetValue("VersionIOS"), "OnVersionIOS")
+	--help
 	local itemHelp = E_MainFrame:AddMainMenuItem((PX2_LM:GetValue("Help")))
 end
 
