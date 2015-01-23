7279cddc14fe347ea89679e4f9d0ade41e96ec14
linkjoy <realmany@163.com>
2015-01-23 11:44:59 +0800

    add timeline

---
+++ Phoenix3D/Tools/Nirvana/PX2E_TimeLineRender.cpp
@@ -0,0 +1,23 @@
+// PX2E_TimeLineRender.cpp
+
+#include "PX2E_TimeLineRender.hpp"
+using namespace PX2Editor;
+
+IMPLEMENT_DYNAMIC_CLASS(PX2Editor::TimeLineRender, wxWindow)
+BEGIN_EVENT_TABLE(TimeLineRender, wxWindow)
+END_EVENT_TABLE()
+//----------------------------------------------------------------------------
+TimeLineRender::TimeLineRender()
+{
+}
+//----------------------------------------------------------------------------
+TimeLineRender::TimeLineRender(wxWindow *parent) :
+wxWindow(parent, -1)
+{
+	SetBackgroundColour(wxColour(207, 214, 229));
+}
+//----------------------------------------------------------------------------
+TimeLineRender::~TimeLineRender()
+{
+}
+//----------------------------------------------------------------------------
