95c7ff90a92f303d596fe776745017b8350e4f54
linkjoy <realmany@163.com>
2015-01-26 17:32:49 +0800

    add edit define

---
+++ Phoenix3D/PX2Edit/PX2EditDefine.hpp
@@ -0,0 +1,18 @@
+// PX2EditDefine.hpp
+
+#ifndef PX2EDITDEFINE_HPP
+#define PX2EDITDEFINE_HPP
+
+namespace PX2
+{
+
+	enum SelectMode
+	{
+		SM_SINGLE,
+		SM_MULTI,
+		SM_MAX_TYPE
+	};
+
+}
+
+#endif
