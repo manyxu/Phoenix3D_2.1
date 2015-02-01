ef1ab1b211f0d3f01363b1349ce9e1772fc1059c
xuduo <realmany@163.com>
2015-02-01 23:52:09 +0800

    add lost files

---
+++ Phoenix3D/Tools/Nirvana/PX2NirvanaEventType.hpp
@@ -0,0 +1,17 @@
+// PX2NirvanaEventType.hpp
+
+#ifndef PX2NIRVANAUIEVENTTYPE_HPP
+#define PX2NIRVANAUIEVENTTYPE_HPP
+
+#include "PX2EventSystem.hpp"
+
+namespace PX2
+{
+
+	PX2_DECLARE_EVENT_BEGIN(NirvanaEventSpace)
+	PX2_EVENT(TabDrag)
+	PX2_DECLARE_EVENT_END(NirvanaEventSpace)
+
+}
+
+#endif
