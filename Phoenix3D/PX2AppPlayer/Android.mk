0ed98e23ad687a7764e739d32d589428f7ddf690
realmany <promiseto1>
2015-02-13 11:41:26 +0800

    mk

---
+++ Phoenix3D/PX2AppPlayer/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PX2AppPlayer
+
+FILE_LIST := $(wildcard $(LOCAL_PATH)/*cpp)
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/..
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/..\
+$(PX2_ROOT)/PX2SDK/Include/
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+            
+include $(BUILD_STATIC_LIBRARY)
