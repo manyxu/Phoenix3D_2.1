5d32ae4626763402f94c19b91ddc7824acdcdda1
xuduo <realmany@163.com>
2015-03-09 22:05:53 +0800

    add light actor

---
+++ Phoenix3D/PX2Simulation/PX2LightActor.cpp
@@ -0,0 +1,143 @@
+// PX2LightActor.cpp
+
+#include "PX2LightActor.hpp"
+#include "PX2GraphicsRoot.hpp"
+#include "PX2StandardMesh.hpp"
+#include "PX2VertexColor4Material.hpp"
+using namespace PX2;
+
+PX2_IMPLEMENT_RTTI(PX2, Actor, LightActor);
+PX2_IMPLEMENT_STREAM(LightActor);
+PX2_IMPLEMENT_FACTORY(LightActor);
+
+//----------------------------------------------------------------------------
+LightActor::LightActor()
+{
+	mLight = new0 Light(Light::LT_POINT);
+	mLightNode = new0 LightNode(mLight);
+	SetMovable(mLightNode);
+
+	VertexFormat *vf = PX2_GR.GetVertexFormat(GraphicsRoot::VFT_PC);
+
+	StandardMesh stdMesh(vf);
+	stdMesh.SetVertexColor(Float4(1.0f, 1.0f, 0.0f, 1.0f));
+	TriMesh *mesh = stdMesh.Octahedron();
+	PX2::VertexColor4MaterialPtr mtl = new0 VertexColor4Material();
+	mtl->GetWireProperty(0, 0)->Enabled = true;
+	mtl->GetCullProperty(0, 0)->Enabled = false;
+	mesh->LocalTransform.SetUniformScale(0.5f);
+	mesh->SetMaterialInstance(mtl->CreateInstance());
+	CreateHelpNode()->AttachChild(mesh);
+}
+//----------------------------------------------------------------------------
+LightActor::~LightActor()
+{
+}
+//----------------------------------------------------------------------------
+void LightActor::OnAttach()
+{
+	if (mLight) GraphicsRoot::GetSingleton().AddLight(mLight);
+}
+//----------------------------------------------------------------------------
+void LightActor::OnDetach()
+{
+	if (mLight) GraphicsRoot::GetSingleton().RemoveLight(mLight);
+}
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// LightActor
+//----------------------------------------------------------------------------
+void LightActor::RegistProperties()
+{
+	Actor::RegistProperties();
+
+	AddPropertyClass("LightActor");
+}
+//----------------------------------------------------------------------------
+void LightActor::OnPropertyChanged(const PropertyObject &obj)
+{
+	Actor::OnPropertyChanged(obj);
+}
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// 名称支持
+//----------------------------------------------------------------------------
+Object* LightActor::GetObjectByName(const std::string& name)
+{
+	Object* found = Actor::GetObjectByName(name);
+	if (found)
+	{
+		return found;
+	}
+
+	return 0;
+}
+//----------------------------------------------------------------------------
+void LightActor::GetAllObjectsByName(const std::string& name,
+	std::vector<Object*>& objects)
+{
+	Actor::GetAllObjectsByName(name, objects);
+}
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// 持久化支持
+//----------------------------------------------------------------------------
+LightActor::LightActor(LoadConstructor value) :
+Actor(value)
+{
+}
+//----------------------------------------------------------------------------
+void LightActor::Load(InStream& source)
+{
+	PX2_BEGIN_DEBUG_STREAM_LOAD(source);
+
+	Actor::Load(source);
+	PX2_VERSION_LOAD(source);
+
+	PX2_END_DEBUG_STREAM_LOAD(LightActor, source);
+}
+//----------------------------------------------------------------------------
+void LightActor::Link(InStream& source)
+{
+	Actor::Link(source);
+
+	source.ResolveLink(mLight);
+}
+//----------------------------------------------------------------------------
+void LightActor::PostLink()
+{
+	Actor::PostLink();
+}
+//----------------------------------------------------------------------------
+bool LightActor::Register(OutStream& target) const
+{
+	if (Actor::Register(target))
+	{
+		target.Register(mLight);
+
+		return true;
+	}
+	return false;
+}
+//----------------------------------------------------------------------------
+void LightActor::Save(OutStream& target) const
+{
+	PX2_BEGIN_DEBUG_STREAM_SAVE(target);
+
+	Actor::Save(target);
+	PX2_VERSION_SAVE(target);
+
+	PX2_END_DEBUG_STREAM_SAVE(LightActor, target);
+}
+//----------------------------------------------------------------------------
+int LightActor::GetStreamingSize(Stream &stream) const
+{
+	int size = Actor::GetStreamingSize(stream);
+	size += PX2_VERSION_SIZE(mVersion);
+
+	return size;
+}
+//----------------------------------------------------------------------------
---
+++ Phoenix3D/PX2Simulation/PX2LightActor.hpp
@@ -0,0 +1,41 @@
+// PX2LightActor.hpp
+
+#ifndef PX2LIGHTACTOR_HPP
+#define PX2LIGHTACTOR_HPP
+
+#include "PX2Actor.hpp"
+#include "PX2Light.hpp"
+#include "PX2LightNode.hpp"
+
+namespace PX2
+{
+
+	class LightActor : public Actor
+	{
+		PX2_DECLARE_RTTI;
+		PX2_DECLARE_NAMES;
+		PX2_DECLARE_PROPERTY;
+		PX2_DECLARE_STREAM(LightActor);
+
+	public:
+		LightActor();
+		virtual ~LightActor();
+
+		Light *GetLight();
+
+		virtual void OnAttach();
+		virtual void OnDetach();
+
+	protected:
+
+		LightPtr mLight;
+		LightNodePtr mLightNode;
+	};
+
+	PX2_REGISTER_STREAM(LightActor);
+	typedef Pointer0<LightActor> LightActorPtr;
+#include "PX2LightActor.inl"
+
+}
+
+#endif
