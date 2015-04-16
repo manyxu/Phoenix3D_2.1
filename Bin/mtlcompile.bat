74a8f355113353ae1c1c77dd8514264b71015295
realmany <realmany@163.com>
2015-04-16 12:41:51 +0800

    update

--- Bin/mtlcompile.bat
+++ Bin/mtlcompile.bat
@@ -3,10 +3,4 @@
 cd E:\GitHub\Phoenix3D\Bin\Data\engine_mtls\sky\
 ..\\..\\..\\FxCompiler64D.exe sky.xml
 
-cd E:\GitHub\Phoenix3D\Bin\Data\engine_mtls\terrain\
-..\\..\\..\\FxCompiler64D.exe terrain.xml
-
-cd E:\GitHub\Phoenix3D\Bin\Data\engine_mtls\std\
-..\\..\\..\\FxCompiler64D.exe std.xml
-
 pause
